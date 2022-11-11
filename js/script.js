c8e2f86054393ff047e75c71eb220223aadec042
Angela <98893476+amyers0217@users.noreply.github.com>
2022-11-11 15:25:53 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(function () {
+    $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) {
+        var $content = $(this).clone().find('img').css({
+            marginLeft: 0,
+            marginTop: 0,
+            width: '100%',
+            height: 'auto'
+        });
+        e.preventDefault();
+
+        var modal = (function(){                    
+            var $window = $(window);
+            var $modal = $('<div class="modal"></div>');
+            var $header = $('<h3>Photo Gallery</h3>');       
+            var $content = $('<div class="modal-content"></div>');
+            var $close = $('<button role="button" class="modal-close">close</button>'); 
+            
+            $modal.append($header, $content, $close);
+        
+            $close.on('click',function(e){              
+                e.preventDefault();                     
+                modal.close();                          
+            })
+        
+            return {                                    
+                center: function(){                     
+                    var top = Math.max($window.height()-$modal.outerHeight(),0)/2;
+                    var left = Math.max($window.width() -$modal.outerWidth(),0)/2;
+        
+                    $modal.css({                                 
+                        top: top + $window.scrollTop(),         
+                        left: left+ $window.scrollLeft()        
+                    });
+                },
+                open: function(settings) {                           
+                    $content.empty().append(settings.content);      
+        
+                    $modal.css({                                    
+                        width: settings.width || 'auto',
+                        height: settings.height || 'auto',
+                    }).appendTo('body');                           
+        
+                    modal.center();                                 
+                    $(window).on('resize scroll',modal.center);     
+                },
+                close: function(){                                  
+                    $content.empty();                              
+                    $modal.detach();                             
+                    $window.off('resize', modal.center);           
+                }
+            };
+        
+        }())
+    });
+});
+
+
